80eda203ee86e1e7b6b5dc0e4bae6e355226a3bf
tpetiet <thibaultpetiet@gmail.com>
2021-01-30 12:41:49 +0100

    modif classe DishDto

--- src/main/java/com/cicdlectures/menuserver/dto/DishDto.java
+++ src/main/java/com/cicdlectures/menuserver/dto/DishDto.java
@@ -25,8 +25,27 @@ public class DishDto {
 
   private String name;
 
+  public DishDto() {
+  }
+
   public DishDto(Long id, String name) {
+    this.setId(id);
+    this.setName(name);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
     this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
     this.name = name;
   }
 
